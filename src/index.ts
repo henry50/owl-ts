092811b94687f0776681134bd79bc0880ae163a2
Henry <henry50@users.noreply.github.com>
2024-02-13 22:15:59 +0000

    owl-ts now exports required messages

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 export * from "./owl_client.js"
 export * from "./owl_server.js"
+export {
+    RegistrationRequest,
+    UserCredentials,
+    AuthInitRequest,
+    AuthInitResponse,
+    AuthFinishRequest
+} from "./messages.js"
